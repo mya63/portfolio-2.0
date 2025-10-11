27b90b5326afef375f96bc6a0676018addfbe769
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-10-11 07:54:09 +0200

    fix: responsive legal notice

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DOCUMENT } from '@angular/common';
+import { FooterComponent } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [RouterLink],
   templateUrl: './legal-notice.html',
   styleUrls: ['./legal-notice.scss'],
+  imports: [RouterLink, FooterComponent],
 })
-export class LegalNoticeComponent {}
+export class LegalNoticeComponent {
+  private doc = inject(DOCUMENT);
+  backToTop() {
+    this.doc.defaultView?.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+}
