bc0d13df319d7d9e4bc3880268669ca3aea75815
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-10-11 19:58:08 +0200

    fix: buttons hover

--- src/app/core/i18n/translations/en.ts
+++ src/app/core/i18n/translations/en.ts
@@ -1,6 +1,6 @@
 export const en = {
   nav: { about: 'About me', skills: 'Skills', portfolio: 'Portfolio' },
-  hero: { iam: 'I am', role: 'FRONTEND DEVELOPER', talk: "let’s talk!" },
+  hero: { iam: 'I am', role: 'FRONTEND DEVELOPER', talk: "Let’s talk!" },
   skills: { title: 'My skills', cta: { ask: 'Looking for ', more: 'another skill?' } },
   portfolio: { title: 'Portfolio', lead: 'Explore a selection of my work here - Interact with projects to see my skills in action.' },
   contact: { title: 'Contact', send: 'SEND MESSAGE' },
