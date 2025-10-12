7c1601ff719b3e8fe51d02bca96da8ffdf6683e8
Muhammed Yunus Amini <muhammedyunusamini4@gmail.com>
2025-10-12 16:36:25 +0200

    fix: anchor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
-// src/app/app.config.ts (Beispiel)
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',          // <a fragment="..."> scrollt zur ID
+        scrollPositionRestoration: 'enabled' // Zurück-Navigation stellt Scroll-Position wieder her
+      })
+    ),
+  ],
 };
